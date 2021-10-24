f3c4d207d64fdebf082f49e505ec03a55210f68b
sreeram <sramayanam>
2021-10-24 10:24:15 -0500

    Commit8

--- Modules/Nulllabel/variables.tf
+++ Modules/Nulllabel/variables.tf
@@ -125,7 +125,7 @@ variable "label_order" {
 variable "context" {
   type        = string
   description = "A context to append to. Base64 encoded json is expected."
-  default     = "eyJkZWxpbWl0ZXIiOiItIiwibmFtZXMiOlsiYSIsImIiLCJjIl0sInN1ZmZpeGVzIjpbImFiYyIsImRlZiIsImVmZyJdfQ==" # base64ecode(jsonencode({}))
+  default     = "e30=" # base64ecode(jsonencode({}))
 }
 
 variable "additional_tag_map" {
