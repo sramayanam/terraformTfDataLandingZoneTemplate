0e909965eecd0cc286dd0bcbc126c73366a72862
sreeram <sramayanam>
2021-10-28 12:38:37 -0500

    Fixed subnet ID

--- Modules/Network/VirtualNetwork/main.tf
+++ Modules/Network/VirtualNetwork/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_subnet" "subnet1" {
   name                                           = "subnet1"
   resource_group_name                            = var.rg_name
   virtual_network_name                           = azurerm_virtual_network.main.name
-  address_prefixes                               = "10.0.0.0/24"
+  address_prefixes                               = ["10.0.0.0/24"]
   enforce_private_link_endpoint_network_policies = false
 }
 
